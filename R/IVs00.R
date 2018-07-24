6df511cf52dad9f3e09bcd298ad4dffe2ff9b40e
ZTerechshenko <zhanna.terechshenko@gmail.com>
2018-07-24 19:10:54 -0400

    Fifth commit
    Removed netinf_dynamic.rda

---
+++ R/IVs00.R
@@ -0,0 +1,5 @@
+#' This is data that contains independent variables for 2000-2012 years
+#'
+#' @author Zhanna Terechshenko \email{zhanna.terechshenko@gmail.com}
+#'
+"IVs00"
---
+++ R/IVs75.R
@@ -0,0 +1,5 @@
+#' This is data that contains independent variables for 1975-1989 years
+#'
+#' @author Zhanna Terechshenko \email{zhanna.terechshenko@gmail.com}
+#'
+"IVs75"
